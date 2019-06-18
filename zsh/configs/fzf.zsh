06c01e99753eb433882b4bc1f81581306011032f
Wil Hall <wil@wilhall.com>
2019-06-18 19:24:44 -0400

    fzf

--- zsh/configs/fzf.zsh
+++ zsh/configs/fzf.zsh
@@ -4,6 +4,8 @@ bindkey '^F' fzf-completion
 bindkey '^I' $fzf_default_completion
 export FZF_TMUX=1
 export FZF_TMUX_HEIGHT=60%
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_R_OPTS='--sort --exact'
 export FZF_CTRL_T_OPTS="--select-1 --exit-0"
 export FZF_DEFAULT_OPTS='--layout=reverse --preview "[[ -d {} ]] && tree --dirsfirst -alFC {} || bat --theme="OneHalfDark" --style=numbers,changes --color always {}"'

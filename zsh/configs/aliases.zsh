a55128f34bc457d1ba5fdb76a44a218c7cc7a7f7
Wil Hall <wil@wilhall.com>
2019-07-10 12:07:58 -0400

    git kraken

--- zsh/configs/aliases.zsh
+++ zsh/configs/aliases.zsh
@@ -1,6 +1,5 @@
 alias ll="ls -al"
 alias pwr="cd ."
-alias flushmem="echo 'flush_all' | nc localhost 11211"
 
 curl_time() {
     curl -so /dev/null -w "\
@@ -14,9 +13,7 @@ starttransfer:  %{time_starttransfer}s\n\
         total:  %{time_total}s\n" "$@"
 }
 
-function kcd() {
-    cd "/usr/local/var/kurogo/sites/$@"
-}
+alias kraken='open -na GitKraken --args -p "$(git rev-parse --show-toplevel)"'
 
 iterm_tab_color ()
 {
@@ -39,28 +36,3 @@ function quickrebase() {
     git set-upstream
     git fetch origin "$@:$@" && git add -A && git stash save "quickrebase" && git pull && git rebase "$@" && git stash apply stash^{/quickrebase}
 }
-
-function nicedev() {
-    sudo renice -20 $(pgrep php)
-    sudo renice -20 $(pgrep nginx)
-    sudo renice -15 $(pgrep memcached)
-    sudo renice -15 $(pgrep mongo)
-    sudo renice -15 $(pgrep postgres)
-    sudo renice -10 $(pgrep phpstorm)
-    sudo renice -5 $(pgrep Sublime)
-    sudo renice -5 $(pgrep iTerm2)
-    sudo renice -1 $(pgrep "Google Chrome")
-    sudo renice 5 $(pgrep Slack)
-    sudo renice 5 $(pgrep Spark)
-    sudo renice 5 $(pgrep Spotify)
-    sudo renice 5 $(pgrep Todoist)
-    sudo renice 5 $(pgrep CloudApp)
-    sudo renice 5 $(pgrep Tower)
-    sudo renice 5 $(pgrep Dash)
-    sudo renice 5 $(pgrep Finder)
-    sudo renice 5 $(pgrep Divvy)
-    sudo renice 5 $(pgrep 1Password)
-    sudo renice 5 $(pgrep Sip)
-    sudo renice 5 $(pgrep CloudApp)
-}
-

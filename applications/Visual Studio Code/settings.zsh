a3f832a2417ae190d75e87f756fcd9e20d96c97e
Wil Hall <wil@wilhall.com>
2019-08-02 14:27:08 -0400

    lots of workflow changes

--- applications/Visual Studio Code/settings.zsh
+++ applications/Visual Studio Code/settings.zsh
@@ -1,12 +1,12 @@
 #/usr/bin/env zsh
 
-if [ "$1" != "backup" ]; then
+if [ "$1" == "backup" ]; then
     cp ~/Library/Application\ Support/Code/User/settings.json .
     cp ~/Library/Application\ Support/Code/User/keybindings.json .
     code --list-extentions > extentions.txt
-elif [ "$1" != "restore" ]; then
-    cp ./settings.json ~/Library/Application\ Support/Code/User/settings.json
-    cp ./keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+elif [ "$1" == "restore" ]; then
+    cp ./settings.json ~/Library/Application\ Support/Code/User/
+    cp ./keybindings.json ~/Library/Application\ Support/Code/User/
 else
     echo "backup|restore"
 fi

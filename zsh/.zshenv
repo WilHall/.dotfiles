7fa5a21f80ca9105ba346382b6f7519167201c79
Wil Hall <wil@wilhall.com>
2020-01-14 20:11:14 -0500

    cleanup

--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,5 +1,8 @@
 export PATH="$HOME/bin:/usr/sbin:/usr/local/bin:$PATH"
 
+export KEYTIMEOUT=1
+export EDITOR='vim'
+
 # I don't like dogs
 export HUSKY_SKIP_HOOKS=1
 export HUSKY_SKIP_INSTALL=1
--- zsh/configs/fzf.zsh
+++ zsh/configs/fzf.zsh
@@ -1,6 +1,3 @@
-source "$HOME/.fzf/shell/completion.zsh"
-source "$HOME/.fzf/shell/key-bindings.zsh"
-
 export FZF_COMPLETION_TRIGGER='*'
 export FZF_TMUX=1
 export FZF_TMUX_HEIGHT=60%
@@ -9,6 +6,3 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_R_OPTS='--sort --exact'
 export FZF_CTRL_T_OPTS="--select-1 --exit-0"
 export FZF_DEFAULT_OPTS='--layout=reverse --preview "[[ -d {} ]] && tree --dirsfirst -alFC {} || bat --theme="OneHalfDark" --style=numbers,changes --color always {}"'
-
-bindkey '^F' fzf-completion
-bindkey '^I' $fzf_default_completion

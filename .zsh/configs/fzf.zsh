d5f4a7e023e78f7dc2a24e938b4640aa580c63ba
Wil Hall <wil@wilhall.com>
2020-05-06 08:51:48 -0400

    Improvements to fzf preview layout

--- .zsh/configs/fzf.zsh
+++ .zsh/configs/fzf.zsh
@@ -1,8 +1,8 @@
 export FZF_COMPLETION_TRIGGER='*'
 export FZF_TMUX=1
 export FZF_TMUX_HEIGHT=60%
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git --color=always'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_R_OPTS='--sort --exact'
 export FZF_CTRL_T_OPTS="--select-1 --exit-0"
-export FZF_DEFAULT_OPTS='--layout=reverse --preview "[[ -d {} ]] && tree --dirsfirst -alFC {} || bat --theme="OneHalfDark" --style=numbers,changes --color always {}"'
+export FZF_DEFAULT_OPTS='--ansi --layout=reverse --preview "bat --theme="OneHalfDark" --style=full --decorations=always --color always {}"'

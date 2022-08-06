a2ee95897965a6997135a518a9d7d9914fb0ad5d
Wil Hall <wil@wilhall.com>
2022-08-05 21:38:30 -0400

    Remove duplicative hotkey

--- .hammerspoon/window_toggling.lua
+++ .hammerspoon/window_toggling.lua
@@ -18,5 +18,4 @@ end
 hs.hotkey.bind(optshift, "`", function() toggleAppWindow("Finder") end)
 hs.hotkey.bind(cmdshift, "`", function() toggleAppWindow("Mail") end)
 hs.hotkey.bind(hyper, "M", function() toggleAppWindow("Spotify") end)
-hs.hotkey.bind(hyper, "T", function() toggleAppWindow("Linear") end)
 hs.hotkey.bind(hyper, "/", function() toggleAppWindow("Linear") end)

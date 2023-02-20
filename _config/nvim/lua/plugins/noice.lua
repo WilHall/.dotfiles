f2e46cc465b6a63f337c8d35aa008d79f91a46df
Wil Hall <wil@wilhall.com>
2023-02-20 09:58:35 -0500

    Final LSP polish

--- _config/nvim/lua/plugins/noice.lua
+++ _config/nvim/lua/plugins/noice.lua
@@ -12,14 +12,6 @@ return {
       require("noice").setup({
         cmdline = {
           view = "cmdline",
-
-        },
-        lsp = {
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-          },
         },
         presets = {
           bottom_search = true,

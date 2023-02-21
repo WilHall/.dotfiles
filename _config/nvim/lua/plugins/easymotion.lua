d7d10b6758ac70e0d75ecca6f29e5e005fa6ee67
Wil Hall <wil@wilhall.com>
2023-02-21 08:28:05 -0500

    Fix update-dev, incsearch

--- _config/nvim/lua/plugins/easymotion.lua
+++ _config/nvim/lua/plugins/easymotion.lua
@@ -1,46 +1,13 @@
 return {
   {
-    "easymotion/vim-easymotion",
+    "haya14busa/incsearch.vim",
+    "romainl/vim-cool",
     config = function()
       vim.cmd [[
-        function! s:exact_search(...) abort
-          return incsearch#util#deepextend(deepcopy({
-          \   'modules': [incsearch#config#easymotion#module({'overwin': 1})],
-          \   'keymap': {
-          \     "\<CR>": '<Over>(easymotion)'
-          \   },
-          \   'is_expr': 0
-          \ }), get(a:, 1, {}))
-        endfunction
-        
-        function! s:fuzzy_search(...) abort
-          return extend(copy({
-          \   'converters': [incsearch#config#fuzzyword#converter()],
-          \   'modules': [incsearch#config#easymotion#module({'overwin': 1})],
-          \   'keymap': {"\<CR>": '<Over>(easymotion)'},
-          \   'is_expr': 0,
-          \ }), get(a:, 1, {}))
-        endfunction
-        
-        noremap <silent><expr> / incsearch#go(<sid>exact_search())
-        noremap <silent><expr> ? incsearch#go(<sid>fuzzy_search())
-        
-        " <Leader>n{char} to move to {char}
-        map  <leader>n <plug>(easymotion-bd-f)
-        nmap <leader>n <plug>(easymotion-overwin-f)
-        
-        " Move to line
-        map <leader>L <plug>(easymotion-bd-jk)
-        nmap <leader>L <plug>(easymotion-overwin-line)
-        
-        " Move to word
-        map  <leader>w <plug>(easymotion-bd-w)
-        nmap <leader>w <plug>(easymotion-overwin-w)        
+        map /  <Plug>(incsearch-forward)
+        map ?  <Plug>(incsearch-backward)
+        map g/ <Plug>(incsearch-stay)
       ]]
     end,
   },
-  { "haya14busa/incsearch.vim" },
-  { "haya14busa/incsearch-fuzzy.vim" },
-  { "haya14busa/incsearch-easymotion.vim" },
-  { "romainl/vim-cool" },
 }

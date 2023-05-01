c9e2b6dc15ce9a67f9c3763f3126e85c9b4b29df
Wil Hall <wil@wilhall.com>
2023-05-01 14:49:02 -0400

    beep boop

--- _config/nvim/lua/plugins/barbar.lua
+++ _config/nvim/lua/plugins/barbar.lua
@@ -16,34 +16,47 @@ return {
         tabpages = true,
         closable = true,
         clickable = true,
-        diagnostics = {
-          [vim.diagnostic.severity.ERROR] = {
-            enabled = true,
-            icon = ''
-          },
-          [vim.diagnostic.severity.WARN] = {
-            enabled = false,
-            icon = ''
-          },
-          [vim.diagnostic.severity.INFO] = {
-            enabled = false,
-            icon = ''
-          },
-          [vim.diagnostic.severity.HINT] = {
-            enabled = true,
-            icon = ''
-          },
-        },
         highlight_alternate = false,
         highlight_inactive_file_icons = false,
         highlight_visible = true,
-        icons = "both",
+        icons = {
+          buffer_index = true,
+          filetype = { enabled = true },
+          diagnostics = {
+            [vim.diagnostic.severity.ERROR] = {
+              enabled = true,
+              icon = ''
+            },
+            [vim.diagnostic.severity.WARN] = {
+              enabled = false,
+              icon = ''
+            },
+            [vim.diagnostic.severity.INFO] = {
+              enabled = false,
+              icon = ''
+            },
+            [vim.diagnostic.severity.HINT] = {
+              enabled = true,
+              icon = ''
+            },
+          },
+          button = ' ✖',
+          modified = {
+            button = ' ✖',
+          },
+          pinned = {
+            button = '  ',
+          },
+          separator = {
+            left = '‖',
+          },
+          inactive = {
+            separator = {
+              left = '‖',
+            }
+          }
+        },
         icon_custom_colors = false,
-        icon_separator_active = '‖',
-        icon_separator_inactive = '‖',
-        icon_close_tab = ' ✖',
-        icon_close_tab_modified = ' ✖',
-        icon_pinned = '  ',
         insert_at_end = false,
         insert_at_start = false,
         maximum_padding = 1,

815bc65f8d1b299c1a58a8707db202308ac5c11d
Wil Hall <wil@wilhall.com>
2023-11-24 12:01:41 -0500

    just a lil backup

--- .hammerspoon/window_toggling.lua
+++ .hammerspoon/window_toggling.lua
@@ -1,21 +1,24 @@
 function toggleAppWindow(appName)
-   local app = hs.appfinder.appFromName(appName)
-   if app == nil then
-      hs.application.launchOrFocus(appName)
-   else
-      local systemFocusedWindow = hs.window.focusedWindow()
+  local app = hs.appfinder.appFromName(appName)
+  if app == nil then
+    hs.application.launchOrFocus(appName)
+  else
+    local systemFocusedWindow = hs.window.focusedWindow()
 
-      if systemFocusedWindow == app:focusedWindow() then
-        app:hide()
-      else
-        app:unhide()
-        app:activate()
-        app:setFrontmost(true)
-      end
-   end
+    if systemFocusedWindow == app:focusedWindow() then
+      app:hide()
+    else
+      app:unhide()
+      app:activate()
+      app:setFrontmost(true)
+    end
+  end
 end
 
 hs.hotkey.bind(optshift, "`", function() toggleAppWindow("Finder") end)
 hs.hotkey.bind(cmdshift, "`", function() toggleAppWindow("Mail") end)
 hs.hotkey.bind(hyper, "M", function() toggleAppWindow("Spotify") end)
 hs.hotkey.bind(hyper, "/", function() toggleAppWindow("Linear") end)
+hs.hotkey.bind(hyper, "N", function() toggleAppWindow("Notion") end)
+hs.hotkey.bind(hyper, "T", function() toggleAppWindow("TickTick") end)
+hs.hotkey.bind(hyper, "C", function() toggleAppWindow("WebStorm") end)

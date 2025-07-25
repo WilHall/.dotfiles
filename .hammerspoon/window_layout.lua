bfb409af3cbf2d4f8649191a3ac04713f4fe0883
Wil Hall <wil@wilhall.com>
2025-07-25 16:38:48 -0400

    Rewrite hammerspoon window layouts

--- .hammerspoon/window_layout.lua
+++ .hammerspoon/window_layout.lua
@@ -1,29 +1,6 @@
-laptopScreenName = 'Built-in Retina Display'
-
-local positions = {
-  full = { x = 0, y = 0, w = 1, h = 1 },
-  centered = { x = 0.25, y = 0.15, w = 0.5, h = 0.7 },
-  center50 = { x = 0.25, y = 0, w = 0.50, h = 1 },
-  center30Left = { x = 0.25, y = 0, w = 0.30, h = 1 },
-  center50Left = { x = 0.25, y = 0, w = 0.50, h = 1 },
-  center20Right = { x = 0.55, y = 0, w = 0.20, h = 1 },
-  right25b = { x = 0.75, y = 0.5, w = 0.25, h = 0.5 },
-  left25 = { x = 0, y = 0, w = 0.25, h = 1 },
-  left34 = { x = 0, y = 0, w = 0.34, h = 1 },
-  left50 = hs.layout.left50,
-  left66 = { x = 0, y = 0, w = 0.66, h = 1 },
-  right25 = { x = 0.75, y = 0, w = 0.25, h = 1 },
-  right34 = { x = 0.66, y = 0, w = 0.34, h = 1 },
-  right50 = hs.layout.right50,
-  right66 = { x = 0.34, y = 0, w = 0.66, h = 1 },
-  upper50 = { x = 0, y = 0, w = 1, h = 0.5 },
-  lower50 = { x = 0, y = 0.5, w = 1, h = 0.5 },
-  browser = { x = 0, y = 0, w = 0.25, h = 1 },
-  ide = { x = 0.25, y = 0, w = 0.40, h = 1 },
-  terminal = { x = 0.65, y = 0, w = 0.15, h = 1 },
-  messaging = { x = 0.80, y = 0, w = 0.20, h = 1 },
-  messagingHalf= { x = 0.80, y = 0.5, w = 0.20, h = 0.5 },
-}
+-- 
+-- Utility Functions
+--
 
 function find(array, fn)
   for i, v in ipairs(array) do
@@ -45,183 +22,211 @@ function max(t, fn)
   return value
 end
 
-function getWidestScreen()
-  return max(hs.screen.allScreens(), function(a, b) return a:fullFrame().w < b:fullFrame().w end)
+
+-- 
+-- Screen Names (that we know of; used to call out specific screens in physical space)
+--
+
+laptopScreenName = 'Built-in Retina Display'
+externalPrimaryScreenName = 'LG HDR DQHD'
+externalSecondaryScreenName = 'LG ULTRAGEAR+'
+
+-- 
+-- Regions (slots where windows can be)
+--
+
+local regions = {
+  -- By geometric segmentation
+  full = { x = 0, y = 0, w = 1, h = 1 },
+  centered = { x = 0.25, y = 0.15, w = 0.5, h = 0.7 },
+  center50 = { x = 0.25, y = 0, w = 0.50, h = 1 },
+  center30Left = { x = 0.25, y = 0, w = 0.30, h = 1 },
+  center50Left = { x = 0.25, y = 0, w = 0.50, h = 1 },
+  center20Right = { x = 0.55, y = 0, w = 0.20, h = 1 },
+  right25b = { x = 0.75, y = 0.5, w = 0.25, h = 0.5 },
+  left25 = { x = 0, y = 0, w = 0.25, h = 1 },
+  left34 = { x = 0, y = 0, w = 0.34, h = 1 },
+  left50 = hs.layout.left50,
+  left66 = { x = 0, y = 0, w = 0.66, h = 1 },
+  right25 = { x = 0.75, y = 0, w = 0.25, h = 1 },
+  right34 = { x = 0.66, y = 0, w = 0.34, h = 1 },
+  right50 = hs.layout.right50,
+  right66 = { x = 0.34, y = 0, w = 0.66, h = 1 },
+  upper50 = { x = 0, y = 0, w = 1, h = 0.5 },
+  lower50 = { x = 0, y = 0.5, w = 1, h = 0.5 },
+  -- By workspace priority
+  modalPrimary = { x = 0, y = 0, w = 0.30, h = 1 },
+  modalSecondary = { x = 0.30, y = 0, w = 0.50, h = 1 },
+  focalPrimary = { x = 0.25, y = 0, w = 0.40, h = 1 },
+  focalSecondary = { x = 0, y = 0, w = 0.25, h = 1 },
+  peripheralPrimary = { x = 0.65, y = 0, w = 0.15, h = 1 },
+  peripheralSecondary = { x = 0.80, y = 0, w = 0.20, h = 1 },
+  peripheralTertiary = { x = 0.80, y = 0.5, w = 0.20, h = 0.5 },
+}
+
+-- 
+-- Screen Finders
+--
+
+-- The main screen is the screen containing the currently focused window (https://www.hammerspoon.org/docs/hs.screen.html#mainScreen)
+function findMainScreen()
+  return hs.screen.mainScreen()
 end
 
-function getNarrowestScreen()
-  return max(hs.screen.allScreens(), function(a, b) return a:fullFrame().w > b:fullFrame().w end)
-end
-
-function getPrimaryScreen()
+-- The primary screen is the screen designated as the "Main Display" in System Preferences Display settings (https://www.hammerspoon.org/docs/hs.screen.html#primaryScreen)
+function findPrimaryScreen()
   return hs.screen.primaryScreen()
 end
 
-function getScreenLeftOfPrimary()
+function findScreenLeftOfPrimary()
   return hs.screen.find { x = -1, y = 0 }
 end
 
-function getScreenRightOfPrimary()
+function findScreenRightOfPrimary()
   return hs.screen.find { x = 1, y = 0 }
 end
 
-function getLaptopScreen()
-  local screens = hs.screen.allScreens()
-
-  if screens[1]:name() == laptopScreenName then
-    return screens[1]
-  end
-
-  return screens[2]
+function findFirstExtendedScreen()
+  return find(hs.screen.allScreens(), function(screen) return (screen:id() ~= findPrimaryScreen():id()) end)
 end
 
-function getExternalScreen()
-  local screens = hs.screen.allScreens()
-
-  if screens[1]:name() == laptopScreenName then
-    return screens[2]
-  end
-
-  return screens[1]
+function findWidestScreen()
+  return max(hs.screen.allScreens(), function(a, b) return a:fullFrame().w < b:fullFrame().w end)
 end
 
-function undockedMonoLayout()
+function findNarrowestScreen()
+  return max(hs.screen.allScreens(), function(a, b) return a:fullFrame().w > b:fullFrame().w end)
+end
+
+function findLaptopScreen()
+  return find(hs.screen.allScreens(), function(screen) return (screen:name() == laptopScreenName) end)
+
+end
+
+function findExternalPrimaryScreen()
+  return find(hs.screen.allScreens(), function(screen) return (screen:name() == externalPrimaryScreenName) end)
+
+end
+
+function findExternalSecondaryScreen()
+  return find(hs.screen.allScreens(), function(screen) return (screen:name() == externalSecondaryScreenName) end)
+end
+
+-- 
+-- Layouts
+--
+
+-- Macbook Pro undocked with no external screens
+function undockedLayout()
   local laptopScreen = getLaptopScreen()
   return {
-    { "Spotify",         nil, laptopScreen, positions.full,    nil, nil },
-    { "Mail",            nil, laptopScreen, positions.full,    nil, nil },
-    { "Linear",          nil, laptopScreen, positions.full,    nil, nil },
-    { "Messages",        nil, laptopScreen, positions.lower50, nil, nil },
-    { "Slack",           nil, laptopScreen, positions.right66, nil, nil },
-    { "Google Chrome",   nil, laptopScreen, positions.full,    nil, nil },
-    { "Arc",   nil, laptopScreen, positions.full,    nil, nil },
-    { "WebStorm",        nil, laptopScreen, positions.full,    nil, nil },
-    { "RubyMine",        nil, laptopScreen, positions.full,    nil, nil },
-    { "Cursor",        nil, laptopScreen, positions.full,    nil, nil },
-    { "Kaleidoscope",        nil, laptopScreen, positions.full,    nil, nil },
-    { "BambuStudio",     nil, laptopScreen, positions.full,    nil, nil },
-    { "OrcaSlicer",      nil, laptopScreen, positions.full,    nil, nil },
-    { "Adobe Lightroom", nil, laptopScreen, positions.full,    nil, nil },
-    { "Blender",         nil, laptopScreen, positions.full,    nil, nil },
-    { "Copilot",         nil, laptopScreen, positions.full,    nil, nil },
-    { "iTerm2",          nil, laptopScreen, positions.full,    nil, nil },
+    { "Arc", nil, externalScreen, regions.full, nil, nil },
+    { "WebStorm", nil, laptopScreen, regions.full, nil, nil },
+    { "RubyMine", nil, laptopScreen, regions.full, nil, nil },
+    { "Rider", nil, laptopScreen, regions.full, nil, nil },
+    { "Code", nil, laptopScreen, regions.full, nil, nil },
+    { "Fork", nil, laptopScreen, regions.full, nil, nil },
+    { "Kaleidoscope", nil, laptopScreen, regions.full, nil, nil },
+    { "iTerm2", nil, laptopScreen, regions.full, nil, nil },
+    { "BambuStudio", nil, laptopScreen, regions.full, nil, nil },
+    { "OrcaSlicer", nil, laptopScreen, regions.full, nil, nil },
+    { "Adobe Lightroom", nil, laptopScreen, regions.full, nil, nil },
+    { "Blender", nil, laptopScreen, regions.full, nil, nil },
+    { "Silhouette Studio", nil, laptopScreen, regions.full, nil, nil },
+    { "Slack", nil, laptopScreen, regions.right, nil, nil },
+    { "Messages", nil, laptopScreen, regions.rightb25, nil, nil },
+      { "Spotify", nil, laptopScreen, regions.modalPrimary, nil, nil },
+      { "Mail", nil, laptopScreen, regions.modalPrimary, nil, nil },
   }
 end
 
-function dockedMonoLayout()
-  local externalScreen = getExternalScreen()
+-- Macbook Pro undocked with one external screen, typically just above the laptop screen
+function undockedSidecar()
+  local laptopScreen = findLaptopScreen();
+  local externalScreen = findFirstExtendedScreen();
+
   return {
-    { "Spotify",         nil, externalScreen, positions.browser,        nil, nil },
-    { "Mail",            nil, externalScreen, positions.browser,        nil, nil },
-    { "Linear",          nil, externalScreen, positions.browser,        nil, nil },
-    { "Google Chrome",   nil, externalScreen, positions.browser,        nil, nil },
-    { "Arc",   nil, externalScreen, positions.left25,        nil, nil },
-    { "BambuStudio",     nil, externalScreen, positions.center50,      nil, nil },
-    { "OrcaSlicer",      nil, externalScreen, positions.center50,      nil, nil },
-    { "Adobe Lightroom", nil, externalScreen, positions.center50,      nil, nil },
-    { "Blender",         nil, externalScreen, positions.center50,      nil, nil },
-    { "Fork",            nil, externalScreen, positions.center50,      nil, nil },
-    { "Code",            nil, externalScreen, positions.ide,  nil, nil },
-    { "WebStorm",        nil, externalScreen, positions.ide,  nil, nil },
-    { "RubyMine",        nil, externalScreen, positions.ide,  nil, nil },
-    { "Cursor",        nil, externalScreen, positions.ide,  nil, nil },
-    { "Kaleidoscope",        nil, externalScreen, positions.center50Left,  nil, nil },
-    { "iTerm2",          nil, externalScreen, positions.terminal, nil, nil },
-    { "Messages",        nil, externalScreen, positions.messagingHalf,      nil, nil },
-    { "Slack",           nil, externalScreen, positions.messaging,       nil, nil },
+    -- Laptop
+    { "WebStorm", nil, laptopScreen, regions.full, nil, nil },
+    { "RubyMine", nil, laptopScreen, regions.full, nil, nil },
+    { "Rider", nil, laptopScreen, regions.full, nil, nil },
+    { "Code", nil, laptopScreen, regions.full, nil, nil },
+    { "Fork", nil, laptopScreen, regions.full, nil, nil },
+    { "Kaleidoscope", nil, laptopScreen, regions.full, nil, nil },
+    { "iTerm2", nil, laptopScreen, regions.full, nil, nil },
+    { "BambuStudio", nil, laptopScreen, regions.full, nil, nil },
+    { "OrcaSlicer", nil, laptopScreen, regions.full, nil, nil },
+    { "Adobe Lightroom", nil, laptopScreen, regions.full, nil, nil },
+    { "Blender", nil, laptopScreen, regions.full, nil, nil },
+    { "Silhouette Studio", nil, laptopScreen, regions.full, nil, nil },
+    { "Slack", nil, laptopScreen, regions.right, nil, nil },
+    { "Messages", nil, laptopScreen, regions.rightb25, nil, nil },
+      { "Spotify", nil, laptopScreen, regions.modalPrimary, nil, nil },
+      { "Mail", nil, laptopScreen, regions.modalPrimary, nil, nil },
+
+    -- External
+    { "Arc", nil, externalScreen, regions.full, nil, nil },
   }
 end
 
-function dockedClamshellPipArrangementLeft()
-  local newPrimary = getWidestScreen()
-  local newRight = find(hs.screen.allScreens(), function(screen) return (screen:id() ~= newPrimary:id()) end)
-  local newLeft = find(hs.screen.allScreens(),
-    function(screen) return (screen:id() ~= newPrimary:id()) and (screen:id() ~= newRight:id()) end)
-  local primaryFrame = newPrimary:fullFrame()
-  local leftFrame = newLeft:fullFrame()
-  local rightFrame = newRight:fullFrame()
-  newPrimary:setPrimary()
-  newLeft:setOrigin(primaryFrame.x - leftFrame.w, primaryFrame.y)
-  newRight:setOrigin(primaryFrame.w + rightFrame.w, primaryFrame.y)
-end
-
-function dockedClamshellPipArrangementRight()
-  local newPrimary = getWidestScreen()
-  local newLeft = find(hs.screen.allScreens(), function(screen) return (screen:id() ~= newPrimary:id()) end)
-  local newRight = find(hs.screen.allScreens(),
-    function(screen) return (screen:id() ~= newPrimary:id()) and (screen:id() ~= newLeft:id()) end)
-  local primaryFrame = newPrimary:fullFrame()
-  local leftFrame = newLeft:fullFrame()
-  local rightFrame = newRight:fullFrame()
-  newPrimary:setPrimary()
-  newLeft:setOrigin(primaryFrame.x - leftFrame.w, primaryFrame.y)
-  newRight:setOrigin(primaryFrame.w + rightFrame.w, primaryFrame.y)
-end
-
-function triScreenArrangementLeft()
-  local newPrimary = getNarrowestScreen()
-  local newRight = find(hs.screen.allScreens(), function(screen) return (screen:id() ~= newPrimary:id()) end)
-  local newLeft = find(hs.screen.allScreens(),
-    function(screen) return (screen:id() ~= newPrimary:id()) and (screen:id() ~= newRight:id()) end)
-  local primaryFrame = newPrimary:fullFrame()
-  local leftFrame = newLeft:fullFrame()
-  local rightFrame = newRight:fullFrame()
-  newPrimary:setPrimary()
-  newLeft:setOrigin(primaryFrame.x - leftFrame.w, primaryFrame.y)
-  newRight:setOrigin(primaryFrame.w + rightFrame.w, primaryFrame.y)
-end
-
-function triScreenArrangementRight()
-  local newPrimary = getNarrowestScreen()
-  local newLeft = find(hs.screen.allScreens(), function(screen) return (screen:id() ~= newPrimary:id()) end)
-  local newRight = find(hs.screen.allScreens(),
-    function(screen) return (screen:id() ~= newPrimary:id()) and (screen:id() ~= newLeft:id()) end)
-  local primaryFrame = newPrimary:fullFrame()
-  local leftFrame = newLeft:fullFrame()
-  local rightFrame = newRight:fullFrame()
-  newPrimary:setPrimary()
-  newLeft:setOrigin(primaryFrame.x - leftFrame.w, primaryFrame.y)
-  newRight:setOrigin(primaryFrame.w + rightFrame.w, primaryFrame.y)
-end
-
-function dockedClamshellPipLayout()
-  local primaryScreen = getPrimaryScreen();
-  local leftScreen = getScreenLeftOfPrimary();
+-- Macbook Pro undocked with two external screens, typically one to each side of the laptop screen
+function undockedTriScreen()
+  local laptopScreen = findLaptopScreen();
+  local leftScreen = findSCreenLef();
   local rightScreen = getScreenRightOfPrimary();
+
   return {
-    { "Spotify",       nil, leftScreen,    hs.layout.full,    nil, nil },
-    { "Mail",          nil, leftScreen,    hs.layout.full,    nil, nil },
-    { "Linear",        nil, leftScreen,    hs.layout.full,    nil, nil },
-    { "Messages",      nil, rightScreen,   positions.lower50, nil, nil },
-    { "Slack",         nil, rightScreen,   positions.full,    nil, nil },
-    { "Google Chrome", nil, leftScreen,    positions.full,    nil, nil },
-    { "Arc", nil, leftScreen,    positions.full,    nil, nil },
-    { "Linear",        nil, leftScreen,    positions.full,    nil, nil },
-    { "WebStorm",      nil, primaryScreen, positions.full,    nil, nil },
-    { "RubyMine",      nil, primaryScreen, positions.full,    nil, nil },
-    { "Cursor",      nil, primaryScreen, positions.full,    nil, nil },
-    { "Kaleidoscope",      nil, primaryScreen, positions.full,    nil, nil },
-    { "iTerm2",        nil, primaryScreen, positions.full,    nil, nil },
+    -- Left
+    { "Arc", nil, leftScreen, regions.full, nil, nil },
+
+    -- Laptop
+    { "WebStorm", nil, laptopScreen, regions.full, nil, nil },
+    { "RubyMine", nil, laptopScreen, regions.full, nil, nil },
+    { "Rider", nil, laptopScreen, regions.full, nil, nil },
+    { "Code", nil, laptopScreen, regions.full, nil, nil },
+    { "Fork", nil, laptopScreen, regions.full, nil, nil },
+    { "Kaleidoscope", nil, laptopScreen, regions.full, nil, nil },
+    { "iTerm2", nil, laptopScreen, regions.full, nil, nil },
+    { "BambuStudio", nil, laptopScreen, regions.full, nil, nil },
+    { "OrcaSlicer", nil, laptopScreen, regions.full, nil, nil },
+    { "Adobe Lightroom", nil, laptopScreen, regions.full, nil, nil },
+    { "Blender", nil, laptopScreen, regions.full, nil, nil },
+    { "Silhouette Studio", nil, laptopScreen, regions.full, nil, nil },
+      { "Spotify", nil, laptopScreen, regions.modalPrimary, nil, nil },
+      { "Mail", nil, laptopScreen, regions.modalPrimary, nil, nil },
+
+    -- Right
+    { "Slack", nil, rightScreen, regions.full, nil, nil },
+    { "Messages", nil, rightScreen, regions.right, nil, nil },
   }
 end
 
-function triScreenLayout()
-  local primaryScreen = getPrimaryScreen();
-  local leftScreen = getScreenLeftOfPrimary();
-  local rightScreen = getScreenRightOfPrimary();
+-- Macbook Pro docked with two external ultrawide screens stacked vertically
+function dockedWithDualVerticalUlrawideLayout()
+  local externalPrimaryScreen = findExternalPrimaryScreen()
+  local externalSecondaryScreen = findExternalSecondaryScreen()
+
   return {
-    { "Spotify",       nil, leftScreen,    hs.layout.full,    nil, nil },
-    { "Mail",          nil, leftScreen,    hs.layout.full,    nil, nil },
-    { "Messages",      nil, rightScreen,   positions.lower50, nil, nil },
-    { "Slack",         nil, rightScreen,   positions.full,    nil, nil },
-    { "Google Chrome", nil, leftScreen,    positions.full,    nil, nil },
-    { "Arc", nil, leftScreen,    positions.full,    nil, nil },
-    { "Linear",        nil, leftScreen,    positions.full,    nil, nil },
-    { "WebStorm",      nil, primaryScreen, positions.full,    nil, nil },
-    { "RubyMine",      nil, primaryScreen, positions.full,    nil, nil },
-    { "Cursor",      nil, primaryScreen, positions.full,    nil, nil },
-    { "Kaleidoscope",      nil, primaryScreen, positions.full,    nil, nil },
-    { "iTerm2",        nil, primaryScreen, positions.full,    nil, nil },
+    -- Ultrawide Lower
+    { "Arc", nil, externalPrimaryScreen, regions.focalSecondary, nil, nil },
+      { "Spotify", nil, externalPrimaryScreen, regions.modalPrimary, nil, nil },
+      { "Mail", nil, externalPrimaryScreen, regions.modalPrimary, nil, nil },
+    { "WebStorm", nil, externalPrimaryScreen, regions.focalPrimary, nil, nil },
+    { "RubyMine", nil, externalPrimaryScreen, regions.focalPrimary, nil, nil },
+    { "Rider", nil, externalPrimaryScreen, regions.focalPrimary, nil, nil },
+    { "Code", nil, externalPrimaryScreen, regions.focalPrimary, nil, nil },
+      { "Fork", nil, externalPrimaryScreen, regions.modalSecondary, nil, nil },
+      { "Kaleidoscope", nil, externalPrimaryScreen, regions.modalSecondary, nil, nil },
+    { "iTerm2", nil, externalPrimaryScreen, regions.peripheralPrimary, nil, nil },
+    { "Slack", nil, externalPrimaryScreen, regions.peripheralSecondary, nil, nil },
+    { "Messages", nil, externalPrimaryScreen, regions.peripheralTertiary, nil, nil },
+
+    -- Ultrawide Upper
+    { "BambuStudio", nil, externalSecondaryScreen, regions.focalPrimary, nil, nil },
+    { "OrcaSlicer", nil, externalSecondaryScreen, regions.focalPrimary, nil, nil },
+    { "Adobe Lightroom", nil, externalSecondaryScreen, regions.focalPrimary, nil, nil },
+    { "Blender", nil, externalSecondaryScreen, regions.focalPrimary, nil, nil },
+    { "Silhouette Studio", nil, externalSecondaryScreen, regions.focalPrimary, nil, nil },
   }
 end
 
@@ -231,22 +236,21 @@ function applyLayout()
   local screens = hs.screen.allScreens()
 
   if numScreens == 1 then
-    if screens[1]:name() == laptopScreenName then
-      hs.alert.show("Applying 'Undocked Mono' Layout")
-      layout = undockedMonoLayout()
+    hs.alert.show("⇝ Layout: Undocked")
+    layout = undockedLayout()
+  elseif numScreens == 2 then
+    local undocked = find(hs.screen.allScreens(), function(screen) return (screen:name() == laptopScreenName) end)
+
+    if unocked then
+      hs.alert.show("⇝ Layout: Undocked Sidecar")
+      layout = undockedSidecar()
     else
-      hs.alert.show("Applying 'Docked Mono' Layout")
-      layout = dockedMonoLayout()
+      hs.alert.show("⇝ Layout: Docked Dual Vertical Ultrawide")
+      layout = dockedWithDualVerticalUlrawideLayout()
     end
   elseif numScreens == 3 then
-    local isTriScreen = find(hs.screen.allScreens(), function(screen) return (screen:name() == "RTK HDR (1)") end)
-    if isTriScreen then
-      hs.alert.show("Applying 'Tri-screen' Layout")
-      layout = triScreenLayout()
-    else
-      hs.alert.show("Applying 'Docked Clamshell PIP' Layout")
-      layout = dockedClamshellPipLayout()
-    end
+    hs.alert.show("⇝ Layout: Tri-screen")
+    layout = undockedTriScreen()
   end
 
   hs.layout.apply(layout)
